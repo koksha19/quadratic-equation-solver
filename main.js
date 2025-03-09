0247b9bc4917be55c8765aa12adaabbb627e054d
Lev <lev.bereza@gmail.com>
2025-03-09 12:40:43 +0200

    Added file mode

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const readline = require('node:readline/promises');
 
 const rl = readline.createInterface({
@@ -6,39 +7,50 @@ const rl = readline.createInterface({
 });
 
 const main = async () => {
-  try {
-    let a = await rl.question('a = ');
-    if (isNaN(Number(a))) {
-      throw new Error(`Expected a valid real number, got ${a} instead`);
-    }
+  let a, b, c;
+  const file = process.argv.slice(2)[0];
 
-    let b = await rl.question('b = ');
-    if (isNaN(Number(b))) {
-      throw new Error(`Expected a valid real number, got ${b} instead`);
-    }
+  if (file) {
+    const content = fs.readFileSync(file, 'utf8');
+    const args = content.split(' ');
+    a = args[0];
+    b = args[1];
+    c = args[2].trim();
+  } else {
+    try {
+      a = await rl.question('a = ');
+      if (isNaN(Number(a))) {
+        throw new Error(`Expected a valid real number, got ${a} instead`);
+      }
 
-    let c = await rl.question('c = ');
-    if (isNaN(Number(c))) {
-      throw new Error(`Expected a valid real number, got ${c} instead`);
-    }
+      b = await rl.question('b = ');
+      if (isNaN(Number(b))) {
+        throw new Error(`Expected a valid real number, got ${b} instead`);
+      }
 
-    rl.write(`Equation is: (${a})x^2 + (${b})x + (${c})`);
-    const result = solveEquation(a, b, c);
-    if (result === -1) {
-      rl.write('There are no roots');
-    } else if (result.length === 1) {
-      rl.write('There is 1 root:');
-      rl.write(`x = ${result[0]}`);
-    } else {
-      rl.write('There are 2 roots:\n');
-      rl.write(`x1 = ${result[0]}\n`);
-      rl.write(`x2 = ${result[1]}`);
+      c = await rl.question('c = ');
+      if (isNaN(Number(c))) {
+        throw new Error(`Expected a valid real number, got ${c} instead`);
+      }
+    } catch (error) {
+      console.error(error);
+      await main();
     }
-    rl.close();
-  } catch (error) {
-    console.error(error);
-    await main();
   }
+
+  rl.write(`Equation is: (${a})x^2 + (${b})x + (${c})\n`);
+  const result = solveEquation(a, b, c);
+  if (result === -1) {
+    rl.write('There are no roots');
+  } else if (result.length === 1) {
+    rl.write('There is 1 root:');
+    rl.write(`x = ${result[0]}`);
+  } else {
+    rl.write('There are 2 roots:\n');
+    rl.write(`x1 = ${result[0]}\n`);
+    rl.write(`x2 = ${result[1]}`);
+  }
+  rl.close();
 };
 
 const solveEquation = (a, b, c) => {
